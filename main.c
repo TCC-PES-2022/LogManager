9e52a86c77d18ad23b2a419c438705c7ed69c58c
otaviocomp <32240652+otaviocomp@users.noreply.github.com>
2022-11-22 19:03:50 -0300

    all tests logger

--- main.c
+++ main.c
@@ -7,9 +7,13 @@ main(void)
 	char *PN = "PN9876";
 
 	/* Examples of use */
-	logger(LG_AUTHENTICATION, "%s %s", login, "authenticated");
-	logger(LG_IMAGE, "%s %s %s %s", login, "transfered", PN, "image");
+	logger(LG_DEBUG, "%s jldsjf", "this is a debug");
+	logger(LG_AUTHENTICATION, "%s %s", login, "Authenticated");
+	logger(LG_IMAGE, "%s %s -- %s %s", login, "transfered", PN, "image");
 	logger(LG_COMMUNICATION_MANAGER, "%s %s %s %s", login, "transfered", PN, "image");
+	logger(LG_UI, "%s", "UI:)");
+	logger(LG_GUI, "%s", "GUI:(");
+	logger(99, "%s", "lsdjf;adjf");
 
 	return EXIT_SUCCESS;
 }
